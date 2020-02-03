8ad5c3b5a4e4d5a354389163d6510a2ce25f129f
Ganeh Pawar <ganesh.pawar@vshift.com>
2020-02-03 17:17:39 +0530

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './index.js',
+  mode: process.env.NODE_ENV,
+  externals: [ nodeExternals() ],
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  }
+}
